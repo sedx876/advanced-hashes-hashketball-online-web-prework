92bfbee2890982ca805d0cac2ac7406ab89fbf7b
Sharon Watkins <swatkins876@gmail.com>
2019-08-13 12:07:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,37 @@
+def game_hash
+  {
+    :away => { :team_name => "Charlotte Hornets",
+               :colors => ["Turquoise", "Purple"],
+               :players => [
+                   { :player_name => "Jeff Adrien",
+                     :number => 4,
+                     :shoe => 18,
+                     :points => 10,
+                     :rebounds => 1,
+                     :assists => 1,
+                     :steals => 2,
+                     :blocks => 7,
+                     :slam_dunks => 2 
+                   },
+                   
+                 ]
+      
+    }
+    
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Write your code here!
 
 
