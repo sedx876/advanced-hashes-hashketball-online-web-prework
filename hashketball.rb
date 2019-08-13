6bbecc7916efeb1c5c33966fbafccceedfb2a0d9
Sharon Watkins <swatkins876@gmail.com>
2019-08-13 12:30:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -111,6 +111,21 @@ def game_hash
     }
     
   }
+  
+  def num_points_scored(players_name)
+    game_hash.each do |place, team|
+      team.each do |attribute, data|
+        if attribute == :players 
+          data.each do |player|
+            if player[:player_name] == players_name 
+              return player[:points]
+            end
+          end
+        end
+      end
+    end
+  end
+  
 
 
 
