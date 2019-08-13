060b22894dfb1022581ae37fa827f6880fec0139
Sharon Watkins <swatkins876@gmail.com>
2019-08-13 12:35:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -126,13 +126,13 @@ def game_hash
     end
   end
   
-  def num_points_scored(players_name)
+  def shoe_size(players_name)
     game_hash.each do |place, team|
       team.each do |attribute, data|
         if attribute == :players 
           data.each do |player|
             if player[:player_name] == players_name 
-              return player[:points]
+              return player[:shoe]
             end
           end
         end
